09e9a8470d965246371c0f40236146842edaba0c
lweixelbraun <lweixelbraun@student.tgm.ac.at>
2024-09-26 15:26:55 +0200

    View und Model für GewinnSpiel namen angepasst

--- GewinnSpielController.java
+++ GewinnSpielController.java
@@ -1,23 +1,18 @@
 import java.awt.*;
-
 public class GewinnSpielController {
-
     private final GewinnspielView view;
     private final GewinnModel model;
     public GewinnSpielController(GewinnModel model, GewinnspielView view) {
         this.view = view;
         this.model = model;
 
-        this.view.gesamtPunkteZahl.setText(String.valueOf(model.getGesamtPunkte()));
-        view.nochmal.addActionListener(e->this.nochmal());
-
+        this.view.gesamtPunkte.setText(String.valueOf(model.getGesamtPunkte()));
+        this.view.nochmaleinmal.addActionListener(e->this.nochmal());
         this.view.spielerZahl.addActionListener(e->this.spielerZahlEntered());
-
     }
     private void spielerZahlEntered() {
         if(!view.spielerZahl.isEditable()) return;
-
-        if(view.spielerZahl.getText().isEmpty()) return; // Keine Eingabe soll zu nichts führen
+        if(view.spielerZahl.getText().isEmpty()) return;
         int spielerZahl;
 
         try{
@@ -28,37 +23,33 @@ public class GewinnSpielController {
             return;
         }
         view.spielerZahl.setEditable(false);
-
         model.berechneComputerZahl();
         int cz = model.getComputerZahl();
         view.setComputerZahl( String.valueOf(cz));
-
         model.berechneRunde(spielerZahl);
         int rundenErgebnis = model.getRundenErgebnis();
-        view.punkteErgebnis.setText( String.valueOf(rundenErgebnis) );
-
+        view.rundenErgebnis.setText(String.valueOf(rundenErgebnis) );
         int gesamtPunkte = model.getGesamtPunkte();
+
         if(gesamtPunkte <= 0) {
-            view.gesamtPunkteZahl.setText("VERLOREN");
-            view.gesamtPunkteZahl.setBackground(Color.RED);
+            view.gesamtPunkte.setText("VERLOREN");
+            view.gesamtPunkte.setBackground(Color.RED);
         }
-
         if(gesamtPunkte >= 100) {
-            view.gesamtPunkteZahl.setText("GEWONNEN");
-            view.gesamtPunkteZahl.setBackground(Color.GREEN);
+            view.gesamtPunkte.setText("GEWONNEN");
+            view.gesamtPunkte.setBackground(Color.GREEN);
         }
-
         if(rundenErgebnis > 0){
-            view.punkteErgebnis.setBackground(Color.GREEN);
+            view.rundenErgebnis.setBackground(Color.GREEN);
         }
         else{
-            view.punkteErgebnis.setBackground(Color.RED);
+            view.rundenErgebnis.setBackground(Color.RED);
         }
     }
     public void nochmal() {
-        System.out.println("Der Button, wurde geclickt ;-)");
+        System.out.println("Der Button, wurde geclickt");
         view.spielerZahl.setText("");
         view.spielerZahl.setEditable(true);
-        view.punkteErgebnis.setBackground(null);
+        view.rundenErgebnis.setBackground(null);
     }
 }
--- GewinnspielView.java
+++ GewinnspielView.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-
 public class GewinnspielView  extends JFrame{
     JLabel punkte, ergebnis, gesamtPunkte, rundenErgebnis, computerZahl;
     JTextField spielerZahl;
@@ -9,10 +8,9 @@ public class GewinnspielView  extends JFrame{
         super("Gewinnspiel");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-
+        setLayout(new BorderLayout());
         JPanel topPanel = new JPanel();
-        topPanel.setLayout(new GridLayout(2, 2));
+        topPanel.setLayout(new GridLayout(2,2));
 
         punkte = new JLabel("Punkte gesamt:");
         ergebnis = new JLabel("Aktuelles Ergebnis: ");
@@ -20,29 +18,28 @@ public class GewinnspielView  extends JFrame{
         rundenErgebnis = new JLabel("0");
         gesamtPunkte.setOpaque(true);
         rundenErgebnis.setOpaque(true);
+
         topPanel.add(punkte);
         topPanel.add(ergebnis);
         topPanel.add(gesamtPunkte);
         topPanel.add(rundenErgebnis);
-
-        add(topPanel);
+        add(topPanel, BorderLayout.NORTH);
 
         JPanel middlePanel = new JPanel();
         middlePanel.setLayout(new GridLayout(2, 2));
-
         middlePanel.add(new JLabel("Zahl für nächste Runde: "));
         middlePanel.add(new JLabel(" Computerzahl:"));
         spielerZahl = new JTextField(1);
         computerZahl = new JLabel("");
+
         middlePanel.add(spielerZahl);
         middlePanel.add(computerZahl);
-        add(middlePanel);
-
+        add(middlePanel, BorderLayout.CENTER);
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new FlowLayout());
         nochmaleinmal = new JButton("Noch einmal!");
         bottomPanel.add(nochmaleinmal);
-        add(bottomPanel);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
     public void setComputerZahl(String zahl){
         computerZahl.setText(zahl);
